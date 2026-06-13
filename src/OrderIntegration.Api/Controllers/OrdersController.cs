79c2ae894c5dd6244bba554864cc2d389c5acf67
beto13 <enguelbertg13@gmail.com>
2026-06-13 14:26:59 -0300

    commit getorders

--- src/OrderIntegration.Api/Controllers/OrdersController.cs
+++ src/OrderIntegration.Api/Controllers/OrdersController.cs
@@ -1,6 +1,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using OrderIntegration.Application.Features.Orders.Commands.CreateOrder;
+using OrderIntegration.Application.Features.Orders.Queries.GetOrderByExternalId;
+using OrderIntegration.Application.Features.Orders.Queries.GetOrderById;
 
 namespace OrderIntegration.Api.Controllers;
 
@@ -16,18 +18,59 @@ public class OrdersController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommand command,CancellationToken cancellationToken)
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommand command, CancellationToken cancellationToken)
     {
         var result = await _sender.Send(command, cancellationToken);
 
         if (result.IsFailure)
         {
-            return BadRequest(new
+            return Conflict(new
             {
-                error = result.Error
+                code = "ORDER_ALREADY_EXISTS",
+                message = result.Error,
+                traceId = HttpContext.TraceIdentifier
             });
         }
 
         return Accepted(result.Value);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(new GetOrderByIdQuery(id), cancellationToken);
+
+
+        if (result.IsFailure)
+        {
+            return NotFound(new
+            {
+                code = "ORDER_NOT_FOUND",
+                message = result.Error,
+                traceId = HttpContext.TraceIdentifier
+            });
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("external/{externalOrderId}")]
+    public async Task<IActionResult> GetByExternalOrderId(
+    string externalOrderId,
+    CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(new GetOrderByExternalIdQuery(externalOrderId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return NotFound(new
+            {
+                code = "ORDER_NOT_FOUND",
+                message = result.Error,
+                traceId = HttpContext.TraceIdentifier
+            });
+        }
+
+        return Ok(result.Value);
     }
 }
